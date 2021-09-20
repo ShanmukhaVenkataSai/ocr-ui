ff58a49079f799fb2e89320ed30787698254a040
ShanmukhaVenkataSai <pshanmukhavenkatasai@gmail.com>
2021-09-20 09:59:06 +0530

    decrease modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ImageCropperModule } from 'ngx-image-cropper';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HttpClientModule} from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http'
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     ImageCropperModule,
     HttpClientModule
   ],
